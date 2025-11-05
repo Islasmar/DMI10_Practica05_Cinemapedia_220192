cca646fc1564bf9b3b47188b8081f84aa7c87eee
Islasmar <marianoislasfajardo@gmail.com>
2025-11-05 00:23:53 -0600

    Subiendo cambios

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,11 +1,10 @@
+import 'package:cinemapedia_220192/presentation/providers/movies/initialLoading_provider.dart';
 import 'package:cinemapedia_220192/presentation/providers/movies/movie_slideshow_provider.dart';
-import 'package:cinemapedia_220192/presentation/widgets/movies/movie_horizontal_listview.dart';
 import 'package:cinemapedia_220192/presentation/widgets/shared/custom_button_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia_220192/presentation/widgets/widgets.dart';
 import 'package:cinemapedia_220192/presentation/providers/movies/movies_providers.dart';
-
 
 class HomeScreen extends StatelessWidget {
   // Nombre estático de la ruta para la navegación
@@ -35,75 +34,98 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   void initState() {
     super.initState();
 
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(); // Carga la primera página de películas al iniciar la pantalla
-    ref.read(popularMoviesProvider.notifier).loadNextPage(); // Carga la primera página de películas al iniciar la pantalla
-    ref.read(topratedMoviesProvider.notifier).loadNextPage(); // Carga la primera página de películas al iniciar la pantalla
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage(); // Carga la primera página de películas al iniciar la pantalla
-    ref.read(mexicanMoviesProvider.notifier).loadNextPage(); // Carga la primera página de películas al iniciar la pantalla
-
+    ref
+        .read(nowPlayingMoviesProvider.notifier)
+        .loadNextPage(); // Carga la primera página de películas al iniciar la pantalla
+    ref
+        .read(popularMoviesProvider.notifier)
+        .loadNextPage(); // Carga la primera página de películas al iniciar la pantalla
+    ref
+        .read(topratedMoviesProvider.notifier)
+        .loadNextPage(); // Carga la primera página de películas al iniciar la pantalla
+    ref
+        .read(upcomingMoviesProvider.notifier)
+        .loadNextPage(); // Carga la primera página de películas al iniciar la pantalla
+    ref
+        .read(mexicanMoviesProvider.notifier)
+        .loadNextPage(); // Carga la primera página de películas al iniciar la pantalla
   }
 
   @override
   Widget build(BuildContext context) {
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if (initialLoading) return FullscreenLoader();
+
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideShowProvider);
     final popular = ref.watch(popularMoviesProvider);
-    final toprated= ref.watch(topratedMoviesProvider);
+    final toprated = ref.watch(topratedMoviesProvider);
     final upcomingMovies = ref.watch(upcomingMoviesProvider);
     final mexicanMovies = ref.watch(mexicanMoviesProvider);
 
+    return CustomScrollView(
+      slivers: [
+        const SliverAppBar(
+          floating: true,
+          flexibleSpace: FlexibleSpaceBar(title: CustomAppbar()),
+        ),
 
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          const CustomAppbar(),
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, index) {
+            return Column(
+              children: [
+                const CustomAppbar(),
 
-          // Solo muestra las primeras 6 películas en el slideshow
-          MovieSlideshow(movies: nowPlayingMovies.take(6).toList()),
+                // Solo muestra las primeras 6 películas en el slideshow
+                MovieSlideshow(movies: nowPlayingMovies.take(6).toList()),
 
-          MovieHorizontalListview(
-            movies: nowPlayingMovies,
-            title: 'En cines',
-            subTitle: 'Miércoles, 22 de Octubre',
-            loadNextPage: () =>
-                ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
-          ),
+                MovieHorizontalListview(
+                  movies: nowPlayingMovies,
+                  title: 'En cines',
+                  subTitle: 'Miércoles, 22 de Octubre',
+                  loadNextPage: () => ref
+                      .read(nowPlayingMoviesProvider.notifier)
+                      .loadNextPage(),
+                ),
 
-          MovieHorizontalListview(
-            movies: upcomingMovies,
-            title: 'Próximamente',
-            subTitle: 'Noviembre',
-            loadNextPage: () =>
-                ref.read(upcomingMoviesProvider.notifier).loadNextPage(),
-          ),
+                MovieHorizontalListview(
+                  movies: upcomingMovies,
+                  title: 'Próximamente',
+                  subTitle: 'Noviembre',
+                  loadNextPage: () =>
+                      ref.read(upcomingMoviesProvider.notifier).loadNextPage(),
+                ),
 
-          MovieHorizontalListview(
-            movies: popular,
-            title: 'Populares',
-            subTitle: 'Miércoles, 22 de Octubre',
-            loadNextPage: () =>
-                ref.read(popularMoviesProvider.notifier).loadNextPage(),
-          ),
+                MovieHorizontalListview(
+                  movies: popular,
+                  title: 'Populares',
+                  subTitle: 'Miércoles, 22 de Octubre',
+                  loadNextPage: () =>
+                      ref.read(popularMoviesProvider.notifier).loadNextPage(),
+                ),
 
-          MovieHorizontalListview(
-            movies: toprated,
-            title: 'Mejor Calificadas',
-            subTitle: 'Miércoles, 22 de Octubre',
-            loadNextPage: () =>
-                ref.read(topratedMoviesProvider.notifier).loadNextPage(),
-          ),
+                MovieHorizontalListview(
+                  movies: toprated,
+                  title: 'Mejor Calificadas',
+                  subTitle: 'Miércoles, 22 de Octubre',
+                  loadNextPage: () =>
+                      ref.read(topratedMoviesProvider.notifier).loadNextPage(),
+                ),
 
-          MovieHorizontalListview(
-            movies: mexicanMovies,
-            title: 'Picardia Mexicana',
-            subTitle: 'Miércoles, 22 de Octubre',
-            loadNextPage: () =>
-                ref.read(mexicanMoviesProvider.notifier).loadNextPage(),
-          ),
+                MovieHorizontalListview(
+                  movies: mexicanMovies,
+                  title: 'Picardia Mexicana',
+                  subTitle: 'Miércoles, 22 de Octubre',
+                  loadNextPage: () =>
+                      ref.read(mexicanMoviesProvider.notifier).loadNextPage(),
+                ),
 
-          const SizedBox(height: 10,)
-        ],
-      ),
+                const SizedBox(height: 10),
+              ],
+            );
+          }, childCount: 1),
+        ),
+      ],
     );
   }
 }
